aa5a2b25542b438de7a382bae1e2ad2529847b45
uzbekprogrammer <mahmudovabdurahim04@gmail.com>
2021-10-19 09:49:49 +0500

    clear the cash

---
+++ lru_cashe in fibonachchi.py
@@ -0,0 +1,10 @@
+from functools import lru_cache
+#Bu funcsiya ortiqcha cashlarni tozalab dastur tez ishlashiga yordam beradi
+@lru_cache
+def fib(n):
+    #print(f'Calculating {n}')
+    if n<2:
+        return 1
+    return fib(n-1)+fib(n-2)
+a=fib(500)
+print(a)
